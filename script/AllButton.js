6955a7756845e8ec844435cc2cd137192033217e
Nur Mahammad Mondol Robiul <nurmahammadmondolrobiul@gmail.com>
2024-10-03 01:16:04 -0400

    Added Footer Section

--- script/AllButton.js
+++ script/AllButton.js
@@ -18,25 +18,6 @@ const displayCategoriesBtn = categories => {
 
 // *****************************************************
 
-/*
-
-{"status":true,"message":"successfully fetched all the videos","videos":[{"category_id":"1001",
-"video_id":"aaaa",
-
-"thumbnail":"https://i.ibb.co/L1b6xSq/shape.jpg","title":"Shape of You",
-
-"authors":[
-{"profile_picture":"https://i.ibb.co/D9wWRM6/olivia.jpg",
-"profile_name":"Olivia Mitchell",
-"verified":""}
-],
-
-"others":{"views":"100K","posted_date":"16278"},
-
-"description":"Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."},
-
-*/
-
 // video Part Start :
 function AllVideoShow() {
   fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
@@ -55,7 +36,7 @@ const displayCategoriesVideo = videos => {
    <figure class="h-[200px]">
     <img class="h-full w-full object-cover" src=${video.thumbnail} alt="Shoes" />
    </figure>
-   <div class=" flex gap-3 ml-1 my-7">
+   <div class=" flex gap-3 ml-1 my-6">
     <div class="h-10 w-10">
       <img class="h-full w-full rounded-full object-cover" src="${video.authors[0].profile_picture}"/>
     </div>
